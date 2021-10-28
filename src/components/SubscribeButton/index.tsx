bf47772c41f16c8df21cc5774317be4bdffe8372
felix.stenian <stenian.felix@nave.rs>
2021-10-28 11:25:34 -0300

    Pagamentos no stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,36 @@
 import { Button } from './styles';
-
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/srtipe-js';
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
   return (
       <Button
         type="button"
+        onClick={handleSubscribe}
       >
           Subscribe now
       </Button>
